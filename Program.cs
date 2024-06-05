add9431ee85e176eb31acf46df70e8e4d64f9543
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-05 17:02:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Assignment_5._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "RADAR";
+            int L = 0;
+            int R = userInput.Length - 1;
+
+            IsPalindrome(userInput, L, R);
+
+            void IsPalindrome(string input, int L, int R)
+            {
+
+                if (L >= R)
+                {
+                    Console.WriteLine("The String is Palindrome");
+                }
+                else if (input[L].ToString().ToLower() != input[R].ToString().ToLower())
+                {
+                    Console.WriteLine("The String is not a Palindrome");
+                }
+                else
+                {
+                    IsPalindrome(input, L + 1, R - 1);
+                    
+                }
+
+
+            }
+
+
+
+
+        }
+       
+       
+    }
+}
